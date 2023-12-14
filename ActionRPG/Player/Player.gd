ad2897e35afbd69ff7fe4d6a8346db1cd0f63a22
Gabriel De Oliveira <91763970+gdeoliveira03@users.noreply.github.com>
2023-12-13 23:01:41 -0500

    implemented animation tree to finalize run and idle animations

--- ActionRPG/Player/Player.gd
+++ ActionRPG/Player/Player.gd
@@ -6,6 +6,8 @@ const FRICTION = 650
 
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
@@ -15,13 +17,12 @@ func _physics_process(delta):
 	inputVector = inputVector.normalized()
 	
 	if inputVector != Vector2.ZERO:
-		if inputVector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", inputVector)
+		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
