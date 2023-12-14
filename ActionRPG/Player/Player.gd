1d019af9e8881f0bc7606e015b57e8d140036316
Gabriel De Oliveira <91763970+gdeoliveira03@users.noreply.github.com>
2023-12-13 20:52:50 -0500

    implemented acceleration/friction and normalized velocity

--- ActionRPG/Player/Player.gd
+++ ActionRPG/Player/Player.gd
@@ -1,15 +1,23 @@
 extends KinematicBody2D
 
+const MAX_SPEED = 100
+const ACCELERATION = 10
+const FRICTION = 10
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
+	
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	inputVector = inputVector.normalized()
 	
 	if inputVector != Vector2.ZERO:
-		velocity = inputVector
+		velocity += inputVector * (ACCELERATION)
+		velocity = velocity.limit_length(MAX_SPEED)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 	
-	move_and_collide(velocity)
+	print(velocity)
+	move_and_slide(velocity)
